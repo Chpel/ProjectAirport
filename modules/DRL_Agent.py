ed27c125640bf7d660792a9cd7bb5cc71aca319c
Chpel <45126835+Chpel@users.noreply.github.com>
2024-04-21 19:43:16 +0300

    gpu-able

--- modules/DRL_Agent.py
+++ modules/DRL_Agent.py
@@ -25,24 +25,24 @@ from torch import nn,stack
 from torch.optim import Adam
 
 class DispatcherRL(nn.Module):
-    def __init__(self, k_actions, k_agents=1, k_outputs=1):
+    def __init__(self, k_actions, k_agents=1, k_outputs=1, device='cpu'):
         super(DispatcherRL, self).__init__()
         self.k_nns = k_agents
         self.k_actors = k_outputs
         self.k_actions = k_actions
         if k_agents == 1: #Dispatcher
             self.model = nn.Sequential( # 1xfleetx2x9x10
-            nn.Conv3d(k_outputs,8,(2,3,3), stride=1), #1x8x1x7x8
+            nn.Conv3d(k_outputs,8,(2,3,3), stride=1, device=device), #1x8x1x7x8
             nn.ReLU(),
             nn.Flatten(2,3), #1x8x7x8
-            nn.Conv2d(8,16,(3,3), stride=1), #1x16x5x6
+            nn.Conv2d(8,16,(3,3), stride=1, device=device), #1x16x5x6
             nn.ReLU(),
-            nn.Conv2d(16,32,(3,3), stride=1), #1x32x3x5
+            nn.Conv2d(16,32,(3,3), stride=1, device=device), #1x32x3x5
             nn.ReLU(),
-            nn.Conv2d(32,64,(3,3), stride=1), #1x64x1x2
+            nn.Conv2d(32,64,(3,3), stride=1, device=device), #1x64x1x2
             nn.ReLU(),
             nn.Flatten(1),
-            nn.Linear(128, k_actions * k_outputs)) #1x128
+            nn.Linear(128, k_actions * k_outputs, device=device)) #1x128
 
         else:
             pass #Decentralized
